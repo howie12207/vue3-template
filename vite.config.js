f62f63a1a279131af0b88b7879c4957dbae3eee9
Howie <howie12207@gmail.com>
2022-05-28 11:04:14 +0800

    build: add element-plus

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,21 @@ import vue from '@vitejs/plugin-vue';
 
 import { fileURLToPath, URL } from 'url';
 
+// Element plus
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
