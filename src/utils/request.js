53c0684dd579c2d831d3b7f811b50b3207b1cbff
Howie <howie12207@gmail.com>
2022-05-28 13:42:24 +0800

    build: add axios

---
+++ src/utils/request.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+// import { ElMessage } from 'element-plus';
+// import { basePath } from '@/config/apiPath';
+// import { useUserInfoStore } from '@/stores/userInfo';
+
+const service = axios.create({
+    baseURL: 'https://data.gov.tw/api/front/dataset/options-list',
+    timeout: 12000,
+    headers: {},
+});
+
+service.interceptors.request.use(
+    config => {
+        return config;
+    },
+    error => {
+        /* eslint-disable-next-line */
+        console.error(`Axios request error: ${error}`);
+        return Promise.resolve(false);
+    }
+);
+
+service.interceptors.response.use(
+    response => {
+        const res = response.data;
+        return res;
+    },
+    error => {
+        /* eslint-disable-next-line */
+        console.error(`Axios response error: ${error}`);
+        return Promise.resolve(false);
+    }
+);
+
+export default service;
